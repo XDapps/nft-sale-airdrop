bccd332d5d9bd88410eff621da9cf5c8307ba534
XDapps <xdev@xdapps.xyz>
2024-04-13 13:33:29 -0400

    Reorder for gas optimization on revert

--- contracts/CollectionSaleManager.sol
+++ contracts/CollectionSaleManager.sol
@@ -51,9 +51,9 @@ contract CollectionSaleManager is AccessControl {
     }
 
     function activateSale(address _paymentToken, uint256 _priceEach) external MustBeAdmin {
-        require(ICollection(collectionAddress).remainingSupply() > 0, "Collection is not setup");
-        require(_priceEach > 0, "Price Each is invalid");
         require(priceEach == 0, "Sale is already active");
+        require(_priceEach > 0, "Price Each is invalid");
+        require(ICollection(collectionAddress).remainingSupply() > 0, "Collection is not setup");
         paymentToken = _paymentToken;
         priceEach = _priceEach;
         emit SaleActivated(collectionAddress, _paymentToken, priceEach);
